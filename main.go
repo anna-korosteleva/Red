3ff9ea5ccb4866dff8cdae33ccda5e76fe92cb30
anna-korosteleva <annatryapcheva@gmail.com>
2025-08-18 17:20:17 +0500

    Commit 5

--- main.go
+++ main.go
@@ -11,6 +11,11 @@ func main() {
 	log.Println("Job begin")
 	log.Println("Okay")
 
+	log.Println("Job begin1")
+	log.Println("Job begin2")
+	log.Println("Job begin3")
+	log.Println("Job begin4")
+
 	if err := godotenv.Load(); err != nil {
 		log.Printf("Предупреждение: не удалось загрузить .env файл: %v", err)
 	}
